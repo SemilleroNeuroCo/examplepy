d689bbe267ac42112ae62b831ab7ab6466fc51a1
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2021-10-24 23:21:36 -0500

    archivo de instalacion

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='examplepy',
+      version='0.1',
+      description='Solo un paquete de ejemplo',
+      url='https://github.com/SemilleroNeuroCo/examplepy',
+      author='Yorguin Mantilla',
+      author_email='yjmantilla@gmail.com',
+      packages=['examplepy'])
