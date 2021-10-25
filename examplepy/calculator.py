8c8e4bac59b8959d7551e9d95abd83e99f54bce0
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2021-10-25 00:37:11 -0500

    documentacion

--- examplepy/calculator.py
+++ examplepy/calculator.py
@@ -1,17 +1,76 @@
 def suma(x,y):
+    """Sumar dos números x,y
+
+    Parameters
+    ----------
+    x : Numeric
+    y : Numeric
+    
+    Returns
+    -------
+    Numeric : Suma de los números
+    """
     return x+y
 
 def resta(x,y):
+    """Resta dos números x,y
+
+    Parameters
+    ----------
+    x : Numeric
+    y : Numeric
+    
+    Returns
+    -------
+    Numeric : Resta de los números
+    """
+
     return x-y
 
 def multiplicacion(x,y):
+    """Multiplicación dos números x,y
+
+    Parameters
+    ----------
+    x : Numeric
+    y : Numeric
+    
+    Returns
+    -------
+    Numeric : Multiplicación de los números
+    """
+
     return x*y
 
 def division(x,y):
+    """División dos números x,y
+
+    Parameters
+    ----------
+    x : Numeric
+    y : Numeric
+    
+    Returns
+    -------
+    Numeric : División de los números
+    """
+
     if y == 0:
         raise ValueError
 
     return x/y
 
 def power(x,y):
+    """Elevar x a la y
+
+    Parameters
+    ----------
+    x : Numeric
+    y : Numeric
+    
+    Returns
+    -------
+    Numeric : Potencia x**y
+    """
+
     return x**y
