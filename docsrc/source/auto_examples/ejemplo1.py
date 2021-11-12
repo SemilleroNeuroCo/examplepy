f9aad615dbb64b0415478a4d311ccd3a3a88c219
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2021-11-11 23:16:50 -0500

    fix docs name for github pages

---
+++ docsrc/source/auto_examples/ejemplo1.py
@@ -0,0 +1,26 @@
+"""
+Ejemplo 1 de examplepy
+=======================
+"""
+
+import examplepy.calculator as calc
+
+
+print('Hola')
+
+#%%
+# Suma
+# ^^^^^^^^
+
+s = calc.suma(5,1)
+
+print('5+1=',s)
+
+#%%
+# Multiplicacion
+# ^^^^^^^^^^^^^^^^
+
+s = calc.multiplicacion(5,1)
+
+print('5*1=',s)
+
